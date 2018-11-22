8569d0d51b1ea9e377ef3a4fa7df586b61422a81
Cathleen <ca.schmalfuss@live.de>
2018-11-22 11:17:33 +0100

    updated LoginManager

--- EvilCar/EvilCar/BL/LoginManager.cs
+++ EvilCar/EvilCar/BL/LoginManager.cs
@@ -1,3 +1,4 @@
+using EvilCar.DL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,39 +6,35 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace EvilCar.BL {
-    class User {
-        private string username;
-        private string password = null;
-        private string role;
-        
+    class LoginManager {
+        public void login () {
+            string username;
+            string password = null;
 
-        public void register () {
-            Console.WriteLine("Please enter your username:");
-            username = Console.ReadLine();
-            Console.WriteLine("This is your chosen username: " + username);
-            Console.WriteLine("Is that correct? y/n");
-            string answer = Console.ReadLine().ToLower();
-
-            while (!string.Equals(answer, "y")) {
-                if (string.Equals(answer, "n")) {
-                    Console.WriteLine("Please enter your username:");
-                    username = Console.ReadLine();
-                    Console.WriteLine("This is your chosen username: " + username);
-                    Console.WriteLine("Is that correct? y/n");
-                    answer = Console.ReadLine().ToLower();
-                } else {
-                    Console.WriteLine("Please type 'y' if you want to use this username or 'n' if you wish to correct it.");
-                    answer = Console.ReadLine().ToLower();
-                    // Console.WriteLine(answer);
-                }
-
-
-                Console.WriteLine("Please enter a password");
-
+            Console.Write("Please enter your username: ");
+             username = Console.ReadLine();
+            Console.Write("Please enter your password: ");
+            while (true) {
+                var key = System.Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Enter)
+                    break;
+                password += key.KeyChar;
             }
 
-            
+
+
+            Console.WriteLine("Logged in!");
+
+        }
+
+        private void dbLookup (string username, string password) {
+            XmlManager lookup = new XmlManager("Database/user.xml");
+            EvilCarUser user = lookup.getUserInformation();
+        }
+
+        private string decodePassword (string password) {
+            var bytes = System.Convert.FromBase64String(password);
+            return System.Text.Encoding.UTF8.GetString(bytes);
         }
     }
-    
 }
