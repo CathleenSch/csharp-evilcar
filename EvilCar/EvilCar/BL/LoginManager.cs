661b477e3447477362b16939ffe5e66c70471019
Cathleen <ca.schmalfuss@live.de>
2018-11-22 10:22:20 +0100

    added .gitignore

--- EvilCar/EvilCar/BL/LoginManager.cs
+++ EvilCar/EvilCar/BL/LoginManager.cs
@@ -7,18 +7,19 @@ using System.Threading.Tasks;
 namespace EvilCar.BL {
     class User {
         private string username;
+        private string password = null;
         private string role;
         
 
-        protected void register () {
+        public void register () {
             Console.WriteLine("Please enter your username:");
             username = Console.ReadLine();
             Console.WriteLine("This is your chosen username: " + username);
             Console.WriteLine("Is that correct? y/n");
             string answer = Console.ReadLine().ToLower();
 
-            while (!string.Equals(answer, "y") || !string.Equals(answer, "yes")) {
-                if (string.Equals(answer, "n") || string.Equals(answer, "no")) {
+            while (!string.Equals(answer, "y")) {
+                if (string.Equals(answer, "n")) {
                     Console.WriteLine("Please enter your username:");
                     username = Console.ReadLine();
                     Console.WriteLine("This is your chosen username: " + username);
@@ -27,7 +28,12 @@ namespace EvilCar.BL {
                 } else {
                     Console.WriteLine("Please type 'y' if you want to use this username or 'n' if you wish to correct it.");
                     answer = Console.ReadLine().ToLower();
+                    // Console.WriteLine(answer);
                 }
+
+
+                Console.WriteLine("Please enter a password");
+
             }
 
             
